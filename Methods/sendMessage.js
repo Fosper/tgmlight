9be32812e855eb6f07e34a830b15f2decb50d8b7
Fosper <fosper@icloud.com>
2021-01-05 05:03:19 +0300

    minor update

--- Methods/sendMessage.js
+++ Methods/sendMessage.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const tl = require('toolslight')
 const Tgmlight = require('../index.js')
 
@@ -6,16 +7,16 @@ Tgmlight.prototype.sendMessage = function() {
     let result = ''
 
     let parameters = [
-        'chat_id',
-        'text',
-        'parse_mode',
-        'disable_web_page_preview',
-        'disable_notification',
-        'reply_to_message_id',
-        'reply_markup'
+        'chat_id', // default ''
+        'text', // default ''
+        'parse_mode', // default 'Markdown'
+        'disable_web_page_preview', // defauls false
+        'disable_notification', // default false
+        'reply_to_message_id', // default ''
+        'reply_markup' // default []
     ]
 
-    if (tl.isEmpty(this.getMethod())) {
+    if (tl.isEmpty(this.requestOptions.messageType)) {
         this.setMessageType(path.basename(__filename).substr(0, path.basename(__filename).length - 3))
     }
 
