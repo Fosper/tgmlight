1a7027efed3a0ca32f6e60e5a8c82086afbcf4b6
Fosper <fosper@icloud.com>
2021-01-08 14:25:26 +0300

    minor update

--- index.js
+++ index.js
@@ -48,6 +48,88 @@ class Tgmlight {
         return this.requestOptions.messageType
     }
 
+    addKeyboardRow = (value) => {
+        let keyboardType = 'inline_keyboard'
+        if (typeof value[0] === 'string') {
+            keyboardType = 'keyboard'
+        } else {
+            console.log(value[0])
+            console.log(value[0].text)
+            console.log(value[0].requestContact)
+            if (value[0].requestContact !== undefined || value[0].requestLocation !== undefined || value[0].requestPoll !== undefined) {
+                keyboardType = 'keyboard'
+            }
+        }
+
+        let keyboardRow = []
+        for (const button of value) {
+            let key = {}
+            if (typeof button === 'string') {
+                key.text = button
+            } else {
+                for (const buttonField of Object.keys(button)) {
+                    switch (buttonField) {
+                        case 'text':
+                            key.text = button[buttonField]
+                            break;
+                        case 'requestContact':
+                            key.request_contact = button[buttonField]
+                            break
+                        case 'requestLocation':
+                            key.request_location = button[buttonField]
+                            break
+                        case 'requestPoll':
+                            key.request_poll = button[buttonField]
+                            break
+                        case 'url':
+                            key.url = button[buttonField]
+                            break
+                        case 'loginUrl':
+                            key.login_url = button[buttonField]
+                            break
+                        case 'callbackData':
+                            key.callback_data = button[buttonField]
+                            break
+                        case 'switchInlineQuery':
+                            key.switch_inline_query = button[buttonField]
+                            break
+                        case 'switchInlineQueryCurrentChat':
+                            key.switch_inline_query_current_chat = button[buttonField]
+                            break
+                        case 'callbackGame':
+                            key.callback_game = button[buttonField]
+                            break
+                        case 'pay':
+                            key.pay = button[buttonField]
+                            break
+                        default:
+                            key[buttonField] = button[buttonField]
+                            break
+                    }
+                }
+            }
+            keyboardRow.push(key)
+        }
+
+        let replyMarkup = this.getReplyMarkup()
+        if (tl.isEmpty(replyMarkup[keyboardType])) {
+            replyMarkup[keyboardType] = []
+        }
+        replyMarkup[keyboardType].push(keyboardRow)
+
+        this.setReplyMarkup(replyMarkup)
+
+        return this
+
+        // url
+        // login_url
+        // callback_data
+        // switch_inline_query
+        // switch_inline_query_current_chat
+        // callback_game
+        // pay
+    }
+
     /*
         For set telegram methods fields.
     */
@@ -102,8 +184,44 @@ class Tgmlight {
         return this
     }
 
-    getReplyToMessageId = (value) => {
+    getReplyToMessageId = () => {
         return this.requestOptions.reply_to_message_id
+    }
+
+    setResizeKeyboard = (value = true) => {
+        if (tl.isEmpty(this.requestOptions.reply_markup)) {
+            this.requestOptions.reply_markup = {}
+        }
+        this.requestOptions.reply_markup.resize_keyboard = value
+        return this
+    }
+
+    getResizeKeyboard = () => {
+        return this.requestOptions.reply_markup.resize_keyboard
+    }
+
+    setOneTimeKeyboard = (value = true) => {
+        if (tl.isEmpty(this.requestOptions.reply_markup)) {
+            this.requestOptions.reply_markup = {}
+        }
+        this.requestOptions.reply_markup.one_time_keyboard = value
+        return this
+    }
+
+    getOneTimeKeyboard = () => {
+        return this.requestOptions.reply_markup.one_time_keyboard
+    }
+
+    setSelective = (value = true) => {
+        if (tl.isEmpty(this.requestOptions.reply_markup)) {
+            this.requestOptions.reply_markup = {}
+        }
+        this.requestOptions.reply_markup.selective = value
+        return this
+    }
+
+    getSelective = () => {
+        return this.requestOptions.reply_markup.selective
     }
 
     setReplyMarkup = (value) => {
@@ -111,7 +229,10 @@ class Tgmlight {
         return this
     }
 
-    getReplyMarkup = (value) => {
+    getReplyMarkup = () => {
+        if (tl.isEmpty(this.requestOptions.reply_markup)) {
+            this.requestOptions.reply_markup = {}
+        }
         return this.requestOptions.reply_markup
     }
 
@@ -141,7 +262,15 @@ class Tgmlight {
             })
             func = 'get' + func.charAt(0).toUpperCase() + func.slice(1)
             if (!tl.isEmpty(this[func])) {
-                body[parameter] = this[func]()
+
+                switch (func) {
+                    case 'getReplyMarkup':
+                        body[parameter] = JSON.stringify(this[func]())
+                        break
+                    default:
+                        body[parameter] = this[func]()
+                        break
+                }
             }
         }
 
